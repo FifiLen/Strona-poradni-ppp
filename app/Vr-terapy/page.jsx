484c0e24582134176e467c33f53b5d133761eb94
FifiLen <filippraca02@gmail.com>
2023-08-13 22:27:44 +0200

    h

--- app/Vr-terapy/page.jsx
+++ app/Vr-terapy/page.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Vr = () => {
   return (
-    <div className="absolute inset-0 overflow-hidden w-full">
-      <iframe 
-        className="w-full h-full border-none"
-        src='https://server376071.nazwa.pl/wordpress/wpn_magnolia/?page_id=1066'
-      ></iframe>
-    </div>
+    <div className="relative overflow-y-auto h-[calc(100vh-64px)] w-full">
+  <iframe
+    className="w-full min-h-[6900px] border-none"
+    src='https://server376071.nazwa.pl/wordpress/wpn_magnolia/?page_id=1066'
+  ></iframe>
+</div>
   )
 }
 
