4f17d32d4833aff13267a8c9f5114f1545df3bb2
FifiLen <filippraca02@gmail.com>
2024-01-25 11:49:55 +0100

    meta

--- app/page.jsx
+++ app/page.jsx
@@ -3,6 +3,12 @@ import Link from 'next/link';
 import React, { useEffect } from 'react';
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
+export const metadata = {
+  title: 'Poradnia Psychologiczno Pedagogiczna Magnolia',
+  keywords: ['Poradnia psychologiczna', 'Zajęcia WWRD', 'Poradnia Rybnik', 'PPP-Rybnik', 'Psycholog Rybnik', 'Diagnoza'],
+  description: 'Miejsce, w którym osiągniesz sukces i staniesz na szczycie swoich możliwości'
+}
+
 
 const Home = () => {
 
