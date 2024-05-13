e51d160a7029384f2de566d54f59f869bc1e1efc
FifiLen <filippraca02@gmail.com>
2024-05-13 08:10:43 +0200

    import

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,6 +1,7 @@
 import * as React from "react"
+import { cn } from "../../lib/utils"
 
-import { cn } from "/Users/filiplendel/Desktop/Vsc + projects/E-new/lib/utils.js"
+
 
 const Card = React.forwardRef<
   HTMLDivElement,
