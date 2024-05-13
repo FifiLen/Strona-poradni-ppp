5a35a4ddc003965bf2f6cbb129173f8a669b1dac
FifiLen <filippraca02@gmail.com>
2024-05-13 12:18:56 +0200

    stonowanie

--- components/MainPageNav.jsx
+++ components/MainPageNav.jsx
@@ -1,6 +1,6 @@
-'use client'
-import { Fragment, useState, useEffect } from 'react'
-import { Dialog, Disclosure, Popover, Transition } from '@headlessui/react'
+"use client";
+import { Fragment, useState, useEffect } from "react";
+import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
   Bars3Icon,
   XMarkIcon,
@@ -11,64 +11,100 @@ import {
   PlayIcon,
   BuildingOfficeIcon,
   ShieldCheckIcon,
-} from '@heroicons/react/24/outline'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
-import Link from 'next/link'
-import Image from 'next/image'
-import { FaFacebook, FaTiktok, FaYoutube } from 'react-icons/fa6'
-
-
-
-
+} from "@heroicons/react/24/outline";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
+import Image from "next/image";
+import { FaFacebook, FaTiktok, FaYoutube } from "react-icons/fa6";
 
 const products = [
-  { name: 'Nasi Specjaliści', description: 'Dowiedz się więcej o zespole Magnolii', href: '/Kadra', icon: UserGroupIcon },
-  { name: 'Nasze miejsce', description: '', href: '/O_nas', icon: BuildingOfficeIcon },
-  { name: 'Regulamin', description: '', href: '/regulamin', icon: ShieldCheckIcon },
+  {
+    name: "Nasi Specjaliści",
+    description: "Dowiedz się więcej o zespole Magnolii",
+    href: "/Kadra",
+    icon: UserGroupIcon,
+  },
+  {
+    name: "Nasze miejsce",
+    description: "",
+    href: "/O_nas",
+    icon: BuildingOfficeIcon,
+  },
+  {
+    name: "Regulamin",
+    description: "",
+    href: "/regulamin",
+    icon: ShieldCheckIcon,
+  },
   // { name: 'Webinary', description: 'Zapisz się na nasze webinaria', href: '/Webinary', icon: BookOpenIcon },
   // { name: 'Blog', description: 'Zajrzyj na nasz blog o E-uzależnieniach', href: 'https://magnolia-blog-eta.vercel.app', icon: ChatBubbleBottomCenterIcon },
-  { name: 'TikTok', description: 'Zajrzyj na naszego TikToka', href: 'https://www.tiktok.com/@poradnia_magnolia_rybnik', icon: FaTiktok },
-  { name: 'Facebook', description: 'Zajrzyj na naszego Facebooka', href: 'https://www.facebook.com/poradnia.magnolia', icon: FaFacebook },
-  { name: 'YouTube', description: 'Zobacz nasze materiały na YouTubie', href: 'https://www.youtube.com/@PPP-MAGNOLIA', icon: FaYoutube },
-]
+  {
+    name: "TikTok",
+    description: "Zajrzyj na naszego TikToka",
+    href: "https://www.tiktok.com/@poradnia_magnolia_rybnik",
+    icon: FaTiktok,
+  },
+  {
+    name: "Facebook",
+    description: "Zajrzyj na naszego Facebooka",
+    href: "https://www.facebook.com/poradnia.magnolia",
+    icon: FaFacebook,
+  },
+  {
+    name: "YouTube",
+    description: "Zobacz nasze materiały na YouTubie",
+    href: "https://www.youtube.com/@PPP-MAGNOLIA",
+    icon: FaYoutube,
+  },
+];
 
-const callsToAction = [
-  
-]
+const callsToAction = [];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function MainPageNav() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
 
- // Dodajemy efekt, który nasłuchuje na przewijanie strony
- useEffect(() => {
-  const handleScroll = () => {
-    // Ustawiamy stan "scrolled" na true/false w zależności od tego, czy strona została przewinięta
-    setScrolled(window.scrollY > 60);
-  };
+  // Dodajemy efekt, który nasłuchuje na przewijanie strony
+  useEffect(() => {
+    const handleScroll = () => {
+      // Ustawiamy stan "scrolled" na true/false w zależności od tego, czy strona została przewinięta
+      setScrolled(window.scrollY > 60);
+    };
 
-  // Dodajemy nasłuchiwacz zdarzenia przewijania
-  window.addEventListener('scroll', handleScroll);
+    // Dodajemy nasłuchiwacz zdarzenia przewijania
+    window.addEventListener("scroll", handleScroll);
 
-  // Usuwamy nasłuchiwacz, gdy komponent jest odmontowywany, aby uniknąć wycieków pamięci
-  return () => window.removeEventListener('scroll', handleScroll);
-}, []);
+    // Usuwamy nasłuchiwacz, gdy komponent jest odmontowywany, aby uniknąć wycieków pamięci
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <header className={` header ${scrolled ? 'bg-white shadow-lg p-4' : ''}`}>
-      <nav className="mx-auto flex w-full items-center justify-between p-3 lg:px-5" aria-label="Global">
-        <div className={`flex  ${scrolled ? 'scale-75' : ''}`}>
+    <header
+      className={`header ${
+        scrolled ? "bg-white shadow-lg p-4" : "bg-white/25"
+      }`}
+    >
+      <nav
+        className="mx-auto flex w-full items-center justify-between p-3 lg:px-5"
+        aria-label="Global"
+      >
+        <div className={`flex  ${scrolled ? "scale-75" : ""}`}>
           <Link href="/" className=" flex justify-start items-center">
-            <Image src="/assets/LogoMainPage.svg" width={200} height={200} priority={true} alt="Logo poradni" />
+            <Image
+              src="/assets/LogoMainPage.svg"
+              width={200}
+              height={200}
+              priority={true}
+              alt="Logo poradni"
+            />
             <span className="sr-only">Magnolia</span>
-            
           </Link>
         </div>
-        
+
         <div className="flex lg:hidden">
           <button
             type="button"
@@ -79,9 +115,12 @@ export default function MainPageNav() {
             <Bars3Icon className="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        
-        <Popover.Group className={`hidden md:flex-wrap md:mx-5 md:gap-x-6 lg:flex font-[450] lg:gap-x-10 bg-gray-200/70 rounded-md py-2 px-4 ${scrolled ? 'bg-white' : ''}`}>
-         
+
+        <Popover.Group
+          className={`hidden md:flex-wrap md:mx-5 md:gap-x-6 lg:flex font-[450] lg:gap-x-10 bg-gray-200/70 rounded-md py-2 px-4 ${
+            scrolled ? "bg-white" : ""
+          }`}
+        >
           <Link href="/" className="text-sm  leading-6 ">
             Strona główna
           </Link>
@@ -102,16 +141,20 @@ export default function MainPageNav() {
             Terapia VR
           </Link>
 
-          <Link href="/assets/oferta-PPP-Magnolia.pdf" className="text-sm  leading-6 ">
+          <Link
+            href="/assets/oferta-PPP-Magnolia.pdf"
+            className="text-sm  leading-6 "
+          >
             Oferta
           </Link>
-        
-
 
           <Popover className="relative">
             <Popover.Button className="flex items-center gap-x-1 text-sm leading-6 ">
               O nas
-              <ChevronDownIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+              <ChevronDownIcon
+                className="h-5 w-5 flex-none text-gray-400"
+                aria-hidden="true"
+              />
             </Popover.Button>
 
             <Transition
@@ -131,10 +174,16 @@ export default function MainPageNav() {
                       className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm leading-6 hover:bg-gray-50"
                     >
                       <div className="flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                        <item.icon className="h-6 w-6 text-gray-600 group-hover:text-[#921d7f]" aria-hidden="true" />
+                        <item.icon
+                          className="h-6 w-6 text-gray-600 group-hover:text-[#921d7f]"
+                          aria-hidden="true"
+                        />
                       </div>
                       <div className="flex-auto">
-                        <Link href={item.href} className="block font-semibold text-gray-900">
+                        <Link
+                          href={item.href}
+                          className="block font-semibold text-gray-900"
+                        >
                           {item.name}
                           <span className="absolute inset-0" />
                         </Link>
@@ -150,7 +199,10 @@ export default function MainPageNav() {
                       href={item.href}
                       className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
                     >
-                      <item.icon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                      <item.icon
+                        className="h-5 w-5 flex-none text-gray-400"
+                        aria-hidden="true"
+                      />
                       {item.name}
                     </Link>
                   ))}
@@ -158,30 +210,31 @@ export default function MainPageNav() {
               </Popover.Panel>
             </Transition>
           </Popover>
-
         </Popover.Group>
         <div className="hidden lg:flex gap-2 lg:justify-end">
-          
-          <Link href="/Kontakt" className={`text-sm bg-gray-200/70 font-semibold py-2 px-5 rounded-md  leading-6 text-gray-900 ${scrolled ? 'bg-white' : ''}`}>
+          <Link
+            href="/Kontakt"
+            className={`text-sm bg-gray-200/70 font-semibold py-2 px-5 rounded-md  leading-6 text-gray-900 ${
+              scrolled ? "bg-white" : ""
+            }`}
+          >
             Kontakt
           </Link>
-          
         </div>
-        
       </nav>
 
-
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
         <div className="fixed inset-0 z-50" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <Link href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Magnolia</span>
-              <img
-                className="h-8 w-auto"
-                src="/assets/magnolia.png"
-                alt=""
-              />
+              <img className="h-8 w-auto" src="/assets/magnolia.png" alt="" />
             </Link>
             <button
               type="button"
@@ -195,7 +248,6 @@ export default function MainPageNav() {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                
                 <Link
                   href="/"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 mt-5"
@@ -226,7 +278,7 @@ export default function MainPageNav() {
                 >
                   Terapia VR
                 </Link>
-                
+
                 <Link
                   href="/assets/oferta-PPP-Magnolia.pdf"
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
@@ -239,7 +291,10 @@ export default function MainPageNav() {
                       <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                         O nas
                         <ChevronDownIcon
-                          className={classNames(open ? 'rotate-180' : '', 'h-5 w-5 flex-none')}
+                          className={classNames(
+                            open ? "rotate-180" : "",
+                            "h-5 w-5 flex-none"
+                          )}
                           aria-hidden="true"
                         />
                       </Disclosure.Button>
@@ -267,11 +322,10 @@ export default function MainPageNav() {
                   Kontakt
                 </Link>
               </div>
-              
             </div>
           </div>
         </Dialog.Panel>
       </Dialog>
     </header>
-  )
+  );
 }
