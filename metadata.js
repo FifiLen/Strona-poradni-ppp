754ddff5042f54d0293f550ad258f4b8001115ec
FifiLen <filippraca02@gmail.com>
2024-12-05 13:57:38 +0100

    Usunięcie starej aplikacji

--- metadata.js
+++
@@ -1,6 +0,0 @@
-export const metadata = {
-    title: 'Poradnia Psychologiczno Pedagogiczna Magnolia',
-    keywords: ['Poradnia psychologiczna', 'Zajęcia WWRD', 'Poradnia Rybnik', 'PPP-Rybnik', 'Psycholog Rybnik', 'Diagnoza'],
-    description: 'Miejsce, w którym osiągniesz sukces i staniesz na szczycie swoich możliwości'
-  };
-  
